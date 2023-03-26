46ed10c9854438732892735d00138cd7eae53ffb
REDEXCT <121300580+REDEXCT@users.noreply.github.com>
2023-03-26 02:21:04 -0400

    Update api.py

--- Project 2/Ordinal Audio/api.py
+++ Project 2/Ordinal Audio/api.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from fetch_nft_data import fetch_nft_data
 from nft_listing import list_nfts
 from ordinal import create_nft, broadcast_transaction
+from nftMarketplace import get_offers, post_offer, put_offer, delete_offer_endpoint, post_trade
 
 app = Flask(__name__)
 
@@ -20,7 +21,7 @@ def get_nfts(address):
         return jsonify({"success": True, "nfts": nfts})
     except Exception as e:
         return jsonify({"success": False, "error": str(e)})
-        
+
 @app.route('/create-nft', methods=['POST'])
 def create_nft_transaction():
     try:
@@ -35,6 +36,27 @@ def create_nft_transaction():
         return jsonify({"success": True, "txid": txid})
     except Exception as e:
         return jsonify({"success": False, "error": str(e)})
+
+# Marketplace API endpoints
+@app.route('/offers', methods=['GET'])
+def get_all_offers():
+    return get_offers()
+
+@app.route('/offers', methods=['POST'])
+def create_offer():
+    return post_offer()
+
+@app.route('/offers/<offer_id>', methods=['PUT'])
+def update_offer(offer_id):
+    return put_offer(offer_id)
+
+@app.route('/offers/<offer_id>', methods=['DELETE'])
+def remove_offer(offer_id):
+    return delete_offer_endpoint(offer_id)
+
+@app.route('/trades', methods=['POST'])
+def create_trade():
+    return post_trade()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
